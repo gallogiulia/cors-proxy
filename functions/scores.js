0667b8ffecf64140e11b84cfb8b37c464620212e
Giulia Gallo <giulia.arche@gmail.com>
2025-07-23 17:13:47 -0700

    Update scores.js

--- functions/scores.js
+++ functions/scores.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
   const response = await fetch("https://script.google.com/macros/s/AKfycbzKtthISCwjQMUBWZrfYgPlQ2nqTEfOdJw7VO2YVPUNWbwF6UOTtjIH9n-T74caDBgCBA/exec");
-  const data = await response.text();
+  const data = await response.json();
 
   return {
     statusCode: 200,
