5b37cba9d706cd5648dfad52febab4ba9f1cf4b9
Giulia Gallo <giulia.arche@gmail.com>
2025-07-23 17:03:57 -0700

    Created Cors Proxy on Netifly

---
+++ functions/scores.js
@@ -0,0 +1,13 @@
+export async function handler(event, context) {
+  const response = await fetch("https://script.google.com/macros/s/AKfycbzKtthISCwjQMUBWZrfYgPlQ2nqTEfOdJw7VO2YVPUNWbwF6UOTtjIH9n-T74caDBgCBA/exec");
+  const data = await response.text();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    body: data
+  };
+}
